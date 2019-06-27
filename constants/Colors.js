0bdb120b558f0b197ff64bdee2a625428a4d8cf4
SahilSadhwani <sadhwanisahil64@gmail.com>
2019-06-27 10:42:45 +0530

    Added .idea to git ignore

---
+++ constants/Colors.js
@@ -0,0 +1,28 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+
+
+  //local colors
+  primaryColor:'#19182F',
+  accentColor:'#9458EA',
+  placeholderColor:'#989CC1',
+  accentGradientStart:'#8250AF',
+  accentGradientEnd:'#3A2496',
+  PrimaryGradientStart:'#1E2542',
+  PrimaryGradientEnd:'#16162C',
+  headingColor:'#FFF',
+
+
+};
